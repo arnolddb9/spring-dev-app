4cd6249b068d3485e4ab453b36c9e66629ad4fae
arnolddb9 <61253264+arnolddb9@users.noreply.github.com>
2023-04-19 17:03:53 -0600

    Se crean las interfaces

---
+++ src/main/java/com/pfcti/spring/dev/app/repository/CuentaRepository.java
@@ -0,0 +1,8 @@
+package com.pfcti.spring.dev.app.repository;
+
+
+import com.pfcti.spring.dev.app.model.Cuenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CuentaRepository extends JpaRepository<Cuenta,Integer> {
+}
